5c405d6f5e98857363686e11bdb252447fda90c0
Jose Luis Martinez <joseluismartinez1024@gmail.com>
2022-10-18 22:33:12 -0500

    Crear triggers

---
+++ Base de datos/Trigger ProductoPedido.sql
@@ -0,0 +1,21 @@
+﻿CREATE TRIGGER [Trigger_ProductoPedido]
+	ON [dbo].[PedidoDetalle]
+	FOR DELETE
+	AS
+	BEGIN
+		--SET NOCOUNT ON agregado para evitar conjuntos de resultados adicionales
+		-- interferir con las instrucciones SELECT.
+		SET NOCOUNT ON
+
+		-- obtener el identificación del registro eliminado
+	  DECLARE @PedidoID INT, @VlrTotalPedido DECIMAL(18,0)
+	  SELECT @PedidoID = [PedidoID]
+	  FROM DELETED
+
+	  SELECT @VlrTotalPedido = SUM(ValorTotal) FROM PedidoDetalle
+	  WHERE PedidoID = @PedidoID
+
+	  -- Insertar declaraciones para desencadenar aquí
+	  UPDATE Pedido SET [ValorTotal] = @VlrTotalPedido
+	  WHERE Pedido.PedidoID= @PedidoID
+	END
---
+++ Base de datos/Trigger ValorTotal.sql
@@ -0,0 +1,21 @@
+﻿CREATE TRIGGER [Trigger_ValorTotal]
+	ON [dbo].[PedidoDetalle]
+	FOR INSERT, UPDATE
+	AS
+	BEGIN
+		--SET NOCOUNT ON agregado para evitar conjuntos de resultados adicionales
+		-- interferir con las instrucciones SELECT.
+	  SET NOCOUNT ON;
+
+	  -- obtener el último valor de identificación del registro insertado o actualizado
+	  DECLARE @PedidoID INT, @VlrTotalPedido DECIMAL(18,0)
+	  SELECT @PedidoID = [PedidoID]
+	  FROM INSERTED
+
+	  SELECT @VlrTotalPedido = SUM(ValorTotal) FROM PedidoDetalle
+	  WHERE PedidoID = @PedidoID
+
+	  -- Insertar declaraciones para desencadenar aquí
+	  UPDATE Pedido SET [ValorTotal] = @VlrTotalPedido
+	  WHERE Pedido.PedidoID= @PedidoID
+	END
